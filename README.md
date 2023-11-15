f65d8bd66d77577a7721c98a047af2ee61dbcf17
Harris <harrisidrees@gmail.com>
2023-11-16 02:47:59 +1100

    u3237207

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Resubmit-One-Page-Site
+
 React One Page Site Converted to Plain Html/Css/Javascript
+
+# Introduction
+
+The website is made on HTML, CSS and Javascript. HTML contains all the structure of the website, Css is for styling elements and Javascript is used for animation effects and calling third party API to get weather details.
+
+Some key takeaways are:
+Fonts taken from google fonts
+Current Location Fire Danger Functionality is the coolest part of the assignment.
+
+# What I Did Well?
+
+The layouts, which were perfectly built and the css styling along with fonts and making the theme close to geographical themed website.
+Thinking out of box and create weather app using json api
+
+# Things that could have been possibly improved?
+
+The funtionality as per single page website looks great, but can be improved further if we take it to multiple pages. Also there could be implementation of different design ideas of website layouts. The design aspect of website could've been improved.
+Weather app picture is static, it could have been improved to change as per current weather.
+
+# Production Notes
+
+For the Navbar, first of all I have used flex property to add space between two containers that will make one content to the left and other to right. Navbar is set to fixed which means it will appear all the time on the screen.
+
+Then on the video section, I have to research how to add autoplay feature, then i figured out I have to add muted attribute aswell to make it work. Along with that, I have added Video title as a header over the video, whose position is absolute.
+
+Now comes the main section parts. All of the sections required very similar styling, so I have to use all the reusable styling into one css classes to avoid repetetion. For some main containers where I required additional styling, I added seperate css classes for them.
+
+There is a Weather api section where you can check your current location weather from which you can tell if your area is at risk of fire, or its safe. It uses Html geolocation property to access lattitude and longitude which will be required to get weather data from a third-party API known as "OpenWeather". This API returns all the required weather details such as temperature,humiditiy,location. etc. It also sends weather image data, however it comes with image code, that represents weather condition. The image code list is huge, that bring alot of work to implement the dynamic image functionality. So I have used a static cloudy image.
+
+The biggest challenge was to add the fade up effect whenever user scrolls on the section. I have used the intersection observer which observes which element is user viewing currently. If that is true, observer adds the css class that makes fade effect to life. It took alot of time to figure out how to make it work on multiple elements.
---
+++ script.js
@@ -0,0 +1,76 @@
+function reveal() {
+  // Select all elements with the class "reveal"
+  var reveals = document.querySelectorAll(".reveal");
+
+  // Get the height of the window
+  var windowHeight = window.innerHeight;
+
+  // Iterate over each element with the class "reveal"
+  reveals.forEach(function (element) {
+    // Get the distance from the top of the element to the top of the viewport
+    var elementTop = element.getBoundingClientRect().top;
+
+    // Set the threshold for an element to be considered visible
+    var elementVisible = 150;
+
+    // Toggle the "active" class based on visibility
+    element.classList.toggle(
+      "active",
+      elementTop < windowHeight - elementVisible
+    );
+  });
+}
+// Add a scroll event listener to trigger the 'reveal' function during scrolling
+window.addEventListener("scroll", reveal);
+
+// Call the 'reveal' function to check the scroll position on page load
+reveal();
+
+// Function to handle the click event for getting the user's current location
+function handleLocationClick() {
+  // Check if geolocation is supported by the browser
+  if (navigator.geolocation) {
+    // Call the geolocation API to get the current position
+    navigator.geolocation.getCurrentPosition(success);
+  } else {
+    // Log a message if geolocation is not supported
+    console.log("Geolocation not supported");
+  }
+}
+
+// Function to handle the success callback of the geolocation API
+function success(position) {
+  // Get the latitude and longitude from the position object
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  // Log the latitude and longitude to the console
+  console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+  // Make an API call to OpenWeatherMap using the retrieved coordinates
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=c575fb12d915b7e85309eb09abff0f93&units=metric`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Log the weather data to the console
+      console.log(data);
+
+      // Check if data is available
+      if (data) {
+        // Display the weather card and update its content
+        document.getElementById("weather-card").style.display = "block";
+        document.getElementById("weather-name").innerHTML = data.name;
+        document.getElementById("weather-date").innerHTML =
+          new Date().toDateString();
+        document.getElementById("weather-temp").innerHTML =
+          Math.floor(data.main.temp).toString() + "° C";
+        document.getElementById("weather-humidity").innerHTML =
+          data.main.humidity.toString() + " %";
+        document.getElementById("weather-wind").innerHTML =
+          Math.floor(data.wind.speed).toString() + " Km/h";
+      }
+      // Image references from https://openweathermap.org/weather-conditions
+    })
+    .catch((error) => console.log("Cannot get weather at your location"));
+}
